670c32a16173a462f5f9ba394fcbe8acd16bb6be
kim.jeongsun <kim.jeongsun@linecorp.com>
2018-12-18 14:43:42 +0900

    msg

---
+++ src/main/java/quiz/MeaningOfThis.java
@@ -0,0 +1,44 @@
+/*
+ */
+package quiz;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ *
+ *
+ * @author
+ * @since 2018. 12. 18.
+ */
+@Slf4j
+public class MeaningOfThis {
+	public final int value = 4;
+	public void doIt() {
+		int value = 6;
+		Runnable r = new Runnable() {
+			public final int value = 5;
+			@Override
+			public void run() {
+				int value = 10;
+				log.debug("this.value: {}", this.value);
+			}
+		};
+		r.run();
+	}
+
+	public static void main(String... args) {
+		MeaningOfThis m = new MeaningOfThis();
+		m.doIt();
+		m.doItLambdas();
+	}
+
+	public void doItLambdas() {
+		//int value = 6;
+		Runnable r = () -> {
+			final int value = 5;
+			log.debug("this.value: {}", this.value);
+			log.debug("second sentence");
+		};
+		r.run();
+	}
+}
